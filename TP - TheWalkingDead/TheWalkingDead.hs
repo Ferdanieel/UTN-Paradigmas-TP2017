e67169c9c1644da4c1ebe2d0fcd7e59ca1a90ccf
Ferdanieel <daniel_fernandez251@hotmail.com>
2017-04-16 11:08:37 -0300

    Cambie "Personaje" por "Protagonista"

--- TP - TheWalkingDead/TheWalkingDead.hs
+++ TP - TheWalkingDead/TheWalkingDead.hs
@@ -1,12 +1,12 @@
 import Text.Show.Functions
-data Personaje = Personaje Nombre PuntosVida deriving (Show)
+data Protagonista = Protagonista Nombre PuntosVida deriving (Show)
 
 type Nombre = String
 type PuntosVida = Int
 
-daryl = Personaje "daryl" 55
-maggie = Personaje "maggie" 100
-carol = Personaje "carol" 200
+daryl = Protagonista "daryl" 55
+maggie = Protagonista "maggie" 100
+carol = Protagonista "carol" 200
 
 zombieTranqui protagonista = dañoZombieTranqui protagonista
 zombieConCasco protagonista = dañoZombieConCasco protagonista
@@ -16,9 +16,9 @@ matar _ = id
 caer unZombie = (unZombie . unZombie)
 sacrificarse _ = matarProtagonista
 
-dañoZombieTranqui (Personaje unNombre unosPuntosVida) = Personaje unNombre (unosPuntosVida - 10)
-dañoZombieConCasco (Personaje unNombre unosPuntosVida) = Personaje unNombre (div unosPuntosVida 2)
-matarProtagonista (Personaje unNombre unosPuntosVida) =  Personaje unNombre 0
+dañoZombieTranqui (Protagonista unNombre unosPuntosVida) = Protagonista unNombre (unosPuntosVida - 10)
+dañoZombieConCasco (Protagonista unNombre unosPuntosVida) = Protagonista unNombre (div unosPuntosVida 2)
+matarProtagonista (Protagonista unNombre unosPuntosVida) =  Protagonista unNombre 0
 -- Mostrar en consola --
 
 -- 3) (zombieTranqui . zombieConCasco) carol
