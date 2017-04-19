33ed209e9ed0fac6180d962a1864a32b32de42b7
suumoo5 <laureano.cavallo1@gmail.com>
2017-04-19 17:10:35 -0300

    Update TheWalkingDead.hs

--- TP - TheWalkingDead/TheWalkingDead.hs
+++ TP - TheWalkingDead/TheWalkingDead.hs
@@ -1,30 +1,68 @@
 import Text.Show.Functions
 import Data.List
-data Protagonista = Protagonista {
-   nombre :: Nombre,
-   vida :: PuntosVida
-  } deriving (Show)
 
 type Nombre = String
 type PuntosVida = Int
+type ReaccionAnteZombie = Zombie -> Protagonista -> Protagonista -- Este es el TIPO de las acciones (definidas abajo) --
+type SeresQueridos = [Nombre]
 
-daryl = Protagonista {nombre="daryl", vida = 55}
-maggie = Protagonista {nombre="maggie", vida = 100}
-carol = Protagonista {nombre="carol", vida = 200}
+data Protagonista = Protagonista {
+   nombre :: Nombre,
+   vida :: PuntosVida
+   reaccion :: ReaccionAnteZombie
+   seresQueridos :: SeresQueridos
+  } deriving (Show)
+
+
+
+daryl = Protagonista {nombre = "daryl", vida = 55, comerSemillasDeHermitaño 30 , ["carol"] }
+maggie = Protagonista {nombre = "maggie", vida = 100, caer, ["carol", "daryl", "krilin"]}
+carol = Protagonista {nombre = "carol", vida = 200, matar, ["victor"], }
+krilin = Protagonista {nombre = "krilin", vida = 1, sacrificarse, []}
+victor sueiro = Protagonista {nombre = "victor sueiro", vida = 1, sacrificarse, []}
+
+
 
 zombieTranqui protagonista = dañoZombieTranqui protagonista
 zombieConCasco protagonista = dañoZombieConCasco protagonista
 zombieSinDientes = id
+zombieBuenazoAstuto hambre | hambre < seresQueridos = zombieSinDientes
+                           | otherwise = zombieConCasco
+zombieReSacado = zombieTranqui && map zombieTranqui (map . zombieConCasco) seresQueridos --Fijate esto --
+
+
 
 matar _ = id
 caer unZombie = (unZombie . unZombie)
-sacrificarse _ = matarProtagonista
+sacrificarse _ = protagonistaMuerto
 
-dañoZombieTranqui protagonista = vida protagonista - 10
+
+
+dañoZombieTranqui protagonista = vida protagonista - 10 -- Podriamos reemplazarlo a "dañoZombieTranqui" y "dañoZombieConCasco directamente en zombieTranqui y zombieConCasco, respectivamente--
 dañoZombieConCasco protagonista = div (vida protagonista) 2
-matarProtagonista protagonista =  vida protagonista * 0
+protagonistaMuerto protagonista =  vida protagonista * 0
+comerSemillasDeHermitaño cantidadDeSemillas | vida <= 0 = id
+                                            | cantidadDeSemillas <= 10 = vida * 0 + 100 --Funciona si le paso a protagonista por Patter Matcihng????
+                                            | cantidadDeSemillas > 10 = protagonistaMuerto
+
+
 -- Mostrar en consola --
 
 -- 3) (zombieTranqui . zombieConCasco) carol
 -- 5) sacrificarse zombieTranqui carol
 -- 6) caer zombieConCasco (matar zombieSinDientes maggie)
+
+----------------------------------------------------------------------
+
+--2.1) TIPOS
+zombieTranqui :: Protagonista -> Protagonista
+zombieConCasco :: Protagonista -> Protagonista
+zombieSinDientes :: Protagonista -> Protagonista
+matar :: Zombie -> Protagonista -> Protagonista
+caer :: Zombie -> Protagonista -> Protagonista
+sacrificarse :: Zombie -> Protagonista -> Protagonista
+
+--6) HORDAS 
+
+ataqueDeHordas :: -> -> -> -> Protagonista -- Fijate como pensarlo aca
+
