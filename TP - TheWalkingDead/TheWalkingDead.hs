5c56aaa858c627fc695708ff1a988869c6690740
Ferdanieel <daniel_fernandez251@hotmail.com>
2017-04-26 15:54:10 -0300

    Delegue a funciones como diferenciaDeConjuntos
    Para que quede mas expresivo, entendible esta creo que es la ultima version de esta parte, ahora solo falta el bonus

--- TP - TheWalkingDead/TheWalkingDead.hs
+++ TP - TheWalkingDead/TheWalkingDead.hs
@@ -34,21 +34,26 @@ krilin = Protagonista {nombre = "krilin", vida = 1, reaccion = sacrificarse, ami
 victorSueiro = Protagonista {nombre = "victorSueiro", vida = 1, reaccion = sacrificarse, amigos = []}
 
 zombieTranqui :: Zombie
-zombieTranqui unProtagonista | unProtagonista == carol = (reaccion unProtagonista) zombieTranqui (nuevaVida (vida unProtagonista - 10) unProtagonista)
-                             | unProtagonista == daryl = (reaccion unProtagonista) zombieTranqui (nuevaVida (vida unProtagonista - 10) unProtagonista)
-                             | unProtagonista == krilin = (reaccion unProtagonista) zombieTranqui (nuevaVida (vida unProtagonista - 10) unProtagonista)
-                             | unProtagonista == maggie = (reaccion unProtagonista) zombieTranqui (nuevaVida (vida unProtagonista - 10) unProtagonista)
-                             | unProtagonista == victorSueiro = (reaccion unProtagonista) zombieTranqui (nuevaVida (vida unProtagonista - 10) unProtagonista)
-                             | otherwise = nuevaVida (vida unProtagonista - 10) unProtagonista
+zombieTranqui unProtagonista | unProtagonista == carol = (reaccion unProtagonista) zombieTranqui (danioZombieTranqui unProtagonista)
+                             | unProtagonista == daryl = (reaccion unProtagonista) zombieTranqui (danioZombieTranqui unProtagonista)
+                             | unProtagonista == krilin = (reaccion unProtagonista) zombieTranqui (danioZombieTranqui unProtagonista)
+                             | unProtagonista == maggie = (reaccion unProtagonista) zombieTranqui (danioZombieTranqui unProtagonista)
+                             | unProtagonista == victorSueiro = (reaccion unProtagonista) zombieTranqui (danioZombieTranqui unProtagonista)
+                             | otherwise = danioZombieTranqui unProtagonista
+
+danioZombieTranqui :: Protagonista -> Protagonista
+danioZombieTranqui unProtagonista = nuevaVida (vida unProtagonista - 10) unProtagonista
 
 zombieConCasco :: Zombie
-zombieConCasco unProtagonista | unProtagonista == carol = (reaccion unProtagonista) zombieConCasco (nuevaVida (div (vida unProtagonista) 2) unProtagonista)
-                              | unProtagonista == daryl = (reaccion unProtagonista) zombieConCasco (nuevaVida (div (vida unProtagonista) 2) unProtagonista)
-                              | unProtagonista == maggie = (reaccion unProtagonista) zombieConCasco (nuevaVida (div (vida unProtagonista) 2) unProtagonista)
-                              | unProtagonista == krilin = (reaccion unProtagonista) zombieConCasco (nuevaVida (div (vida unProtagonista) 2) unProtagonista)
-                              | unProtagonista == victorSueiro = (reaccion unProtagonista) zombieConCasco (nuevaVida (div (vida unProtagonista) 2) unProtagonista)
-                              |  otherwise = (nuevaVida (div (vida unProtagonista) 2) unProtagonista)
+zombieConCasco unProtagonista | unProtagonista == carol = (reaccion unProtagonista) zombieConCasco (danioZombieConCasco unProtagonista)
+                              | unProtagonista == daryl = (reaccion unProtagonista) zombieConCasco (danioZombieConCasco unProtagonista)
+                              | unProtagonista == maggie = (reaccion unProtagonista) zombieConCasco (danioZombieConCasco unProtagonista)
+                              | unProtagonista == krilin = (reaccion unProtagonista) zombieConCasco (danioZombieConCasco unProtagonista)
+                              | unProtagonista == victorSueiro = (reaccion unProtagonista) zombieConCasco (danioZombieConCasco unProtagonista)
+                              | otherwise = danioZombieConCasco unProtagonista
 
+danioZombieConCasco :: Protagonista -> Protagonista
+danioZombieConCasco unProtagonista = (nuevaVida (div (vida unProtagonista) 2) unProtagonista)
 
 zombieSinDientes :: Zombie
 zombieSinDientes = id
@@ -60,15 +65,23 @@ zombieReSacado :: Zombie
 zombieReSacado unProtagonista  = nuevosAmigos (map (zombieTranqui . zombieConCasco) (amigos unProtagonista)) (zombieTranqui unProtagonista)
 
 pelearAmigos :: Protagonista -> [Protagonista] -> Protagonista
-pelearAmigos unProtagonista unosProtagonistas = nuevosAmigos (amigos unProtagonista \\ unosProtagonistas) unProtagonista
+pelearAmigos unProtagonista unosProtagonistas = nuevosAmigos (diferenciaDeConjuntos (amigos unProtagonista) unosProtagonistas) unProtagonista
+
+diferenciaDeConjuntos :: [Protagonista] -> [Protagonista] -> [Protagonista]
+diferenciaDeConjuntos xs ys = filter (\x -> not (x `elem` ys)) (xs)
 
 comerSemillasDeHermitanio :: Int -> Zombie -> Protagonista -> Protagonista
 comerSemillasDeHermitanio semillas unZombie unProtagonista | vida unProtagonista <= 0 = unProtagonista
-                                                           | vida unProtagonista > 0 && semillas <= 10 = nuevaVida (vida unProtagonista * 0 + 100) unProtagonista
+                                                           | vida unProtagonista > 0 && semillas <= 10 = alterarVidadebidoEfectoSemilla unProtagonista
                                                            | vida unProtagonista > 0 && semillas > 10 = muerte unProtagonista
+
+alterarVidadebidoEfectoSemilla :: Protagonista -> Protagonista
+alterarVidadebidoEfectoSemilla unProtagonista = nuevaVida (vida unProtagonista * 0 + 100) unProtagonista
 
 horda :: Protagonista -> [Zombie] -> Protagonista
 horda unProtagonista unosZombies = foldl (flip ($)) unProtagonista unosZombies
+
+
 
 matar :: Reaccion
 matar _ = id
@@ -77,22 +90,44 @@ caer unZombie = (.) unZombie unZombie
 sacrificarse :: Reaccion
 sacrificarse _ = muerte
 muerte :: Protagonista -> Protagonista
-muerte unProtagonista = nuevaVida (vida unProtagonista * 0) (nuevosAmigos (amigos unProtagonista \\ [krilin,victorSueiro]) unProtagonista)
+muerte unProtagonista = nuevaVida (vida unProtagonista * 0) (nuevosAmigos (diferenciaDeConjuntos (amigos unProtagonista) [krilin,victorSueiro]) unProtagonista)
 
 {- Consultas:
-1) pelear maggie [daryl,carol,victorSueiro]
-Protagonista {nombre = "maggie", vida = 100, reaccion = <function>, amigos = [Protagonista {nombre = "krilin", vida = 1, reaccion = <function>, amigos = []}]}
+1) pelearAmigos maggie [daryl,carol,victorSueiro]
+Protagonista {nombre = "maggie", vida = 100, reaccion = <function>, amigos =
+[Protagonista {nombre = "krilin", vida = 1, reaccion = <function>, amigos = []}]}
 
 2) zombieBuenazoAstuto maggie 5
-Protagonista {nombre = "maggie", vida = 25, reaccion = <function>, amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>, amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>, amigos = []}]},Protagonista {nombre = "daryl", vida = 55, reaccion = <function>, amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>, amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>, amigos = []}]}]},Protagonista {nombre = "krilin", vida = 1, reaccion = <function>, amigos = []}]}
+Protagonista {nombre = "maggie", vida = 25, reaccion = <function>,
+amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>,
+amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>,
+amigos = []}]},Protagonista {nombre = "daryl", vida = 55, reaccion = <function>,
+amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>,
+amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>,
+amigos = []}]}]},Protagonista {nombre = "krilin", vida = 1, reaccion = <function>, amigos = []}]}
 
 3) zombieBuenazoAstuto maggie 2
-Protagonista {nombre = "maggie", vida = 80, reaccion = <function>, amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>, amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>, amigos = []}]},Protagonista {nombre = "daryl", vida = 55, reaccion = <function>, amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>, amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>, amigos = []}]}]},Protagonista {nombre = "krilin", vida = 1, reaccion = <function>, amigos = []}]}
+Protagonista {nombre = "maggie", vida = 80, reaccion = <function>,
+amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>,
+amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>,
+amigos = []}]},Protagonista {nombre = "daryl", vida = 55, reaccion = <function>,
+amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>,
+amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>,
+amigos = []}]}]},Protagonista {nombre = "krilin", vida = 1, reaccion = <function>, amigos = []}]}
 
 4) horda maggie [zombieTranqui,zombieBuenazoAstuto 20,zombieReSacado,zombieBuenazoAstuto 1, zombieTranqui]
-Protagonista {nombre = "maggie", vida = -40, reaccion = <function>, amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>, amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>, amigos = []}]},Protagonista {nombre = "daryl", vida = 0, reaccion = <function>, amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>, amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>, amigos = []}]}]},Protagonista {nombre = "krilin", vida = 0, reaccion = <function>, amigos = []}]}
+Protagonista {nombre = "maggie", vida = -40, reaccion = <function>,
+amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>,
+amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>,
+amigos = []}]},Protagonista {nombre = "daryl", vida = 0, reaccion = <function>,
+amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>,
+amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>,
+amigos = []}]}]},Protagonista {nombre = "krilin", vida = 0, reaccion = <function>, amigos = []}]}
 
 5) filter (\x -> vida x == 0) (amigos (horda maggie [zombieTranqui,zombieBuenazoAstuto 20,zombieReSacado,zombieBuenazoAstuto 1, zombieTranqui]))
-[Protagonista {nombre = "daryl", vida = 0, reaccion = <function>, amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>, amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>, amigos = []}]}]},Protagonista {nombre = "krilin", vida = 0, reaccion = <function>, amigos = []}]
+[Protagonista {nombre = "daryl", vida = 0, reaccion = <function>,
+amigos = [Protagonista {nombre = "carol", vida = 200, reaccion = <function>,
+amigos = [Protagonista {nombre = "victorSueiro", vida = 1, reaccion = <function>,
+amigos = []}]}]},Protagonista {nombre = "krilin", vida = 0, reaccion = <function>, amigos = []}]
 
 -}
